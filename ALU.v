ade04ac8c00bfd685d73ad747382acf9e645933a
SaraSaffari <sarasaffari.1997@gmail.com>
2018-05-10 00:28:19 +0430

    alu is donegit add ALU.vgit add ALU.v!

--- ALU.v
+++ ALU.v
@@ -8,8 +8,8 @@ module ALU(inputA, inputB, carryIn, func, result, carryOut, zero, negetive);
 	always @(*) begin
 		case(func)
 		2'b 00: {carryOut, result} = inputA + inputB + carryIn;
-		2'b 00: result = inputB & inputA;
-		2'b 00: result = inputB | inputA;
+		2'b 01: result = inputB & inputA;
+		2'b 10: result = inputB | inputA;
 		endcase
 		zero = ~(|result);
 		negetive = result[7];
--- ALU_TB.v
+++ ALU_TB.v
@@ -4,25 +4,31 @@ module ALU_TB();
 	wire [7:0] rslt;
 	reg ci;
 	reg [2:0] func;
-	reg co;
-	reg z ;
-	reg n ;	
+	wire co;
+	wire z;
+	wire n;	
 
 	initial repeat (10) #50 clk = ~clk;
 	initial begin
 		#10
-		A = 8'b 011111001;
-		B = 8'b 110000100;
-		co = 0;
-		n = 0 ;
-		z = 0 ;
+		A = 8'b 00000001;
+		B = 8'b 11111110;
 		ci = 1;
 		func = 2'b 00;
 		#80
-		func = 2'b 10;
-		#80
+		A = 8'b 00001110;
+		B = 8'b 11101110;
 		func = 2'b 01;
+		#80
+		A = 8'b 00000001;
+		B = 8'b 11000000;
+		func = 2'b 10;
+		#80 
+		A = 8'b 00000001;
+		B = 8'b 00011110;
+		func = 2'b 00;
 	end
+
 
 	ALU X(
 		.inputA(A),
